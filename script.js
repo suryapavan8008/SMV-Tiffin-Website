7f62c11dc5734dfb3fe465da9436f7b5a9718839
surya <suryach.se@gmail.com>
2026-02-20 13:53:02 +0530

    Update images, add milmker, update location, move admin to navbar

--- script.js
+++ script.js
@@ -7,61 +7,61 @@ const defaultMenuItems = [
     id: "item-1",
     name: "Punugulu",
     description: "Small golden fried balls made from fermented dosa batter.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Bonda.jpg"
+    image: "https://images.pexels.com/photos/13741166/pexels-photo-13741166.jpeg"
   },
   {
     id: "item-2",
     name: "Muntha Masala",
     description: "Street-style mix of puffed rice, groundnuts, onion, and masala spices.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Bhel_puri_chaat.jpg"
+    image: "https://images.pexels.com/photos/35213277/pexels-photo-35213277.jpeg"
   },
   {
     id: "item-3",
     name: "Mirchi Bajji",
     description: "Large green chilli dipped in besan batter and deep fried.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Mirchi-bajji%2C%20India.jpg"
+    image: "https://images.pexels.com/photos/33430563/pexels-photo-33430563.jpeg"
   },
   {
     id: "item-4",
     name: "Bread Omelette",
     description: "Toasted bread with fluffy masala omelette filling.",
-    image: "https://images.unsplash.com/photo-1525351484163-7529414344d8?auto=format&fit=crop&w=900&q=80"
+    image: "https://images.pexels.com/photos/27396342/pexels-photo-27396342.jpeg"
   },
   {
     id: "item-5",
     name: "Egg Burji",
     description: "Scrambled egg mixed with masala spices and savory sauce.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Egg_Bhurji-India.jpg"
+    image: "https://images.pexels.com/photos/25406262/pexels-photo-25406262.jpeg"
   },
   {
     id: "item-6",
     name: "Tomato Round Cutting Salad",
     description: "Round tomato slices topped with masala mix, onions, and coriander.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Tomato%2C%20cucumber%2C%20onion%20salad.jpg"
+    image: "https://images.pexels.com/photos/1639555/pexels-photo-1639555.jpeg"
   },
   {
     id: "item-7",
     name: "Boiled Egg Muntha Masala",
     description: "Muntha masala mixture tossed with chopped boiled egg pieces.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Masala%20boiled%20eggs%20served%20in%20a%20plate.jpg"
+    image: "https://images.pexels.com/photos/29670383/pexels-photo-29670383.jpeg"
   },
   {
     id: "item-8",
     name: "Milmker",
     description:
       "Soaked soya chunks fried in a pan with spices, mixed with batani (peas) gravy, and served garnished with chopped onions and coriander.",
-    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Soya%20chunks%20with%20peas%20curry.jpg"
+    image: "https://images.pexels.com/photos/29658378/pexels-photo-29658378.jpeg"
   }
 ];
 
 function normalizeMenuItems(items) {
-  const blockedNames = new Set(["pani puri", "pungulu"]);
   const canonicalNames = new Set(defaultMenuItems.map((item) => item.name.toLowerCase()));
 
   const customItems = items.filter((item) => {
     if (!item || typeof item !== "object" || typeof item.name !== "string") return false;
+    if (typeof item.id === "string" && /^item-\d+$/.test(item.id)) return false;
     const name = item.name.toLowerCase();
-    return !blockedNames.has(name) && !canonicalNames.has(name);
+    return !canonicalNames.has(name);
   });
 
   return [...defaultMenuItems, ...customItems];
